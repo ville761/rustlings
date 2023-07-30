ffcab37b4a9adda2c7dbdab66cc585979de904af
Ville Uski <ville.uski@iki.fi>
2023-07-30 19:39:26 +0000

    solved quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        2*quantity
+    }
+}
 
 // Don't modify this function!
 #[test]
